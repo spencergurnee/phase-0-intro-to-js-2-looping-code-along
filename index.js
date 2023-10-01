bc1397f2c6858a941fb8f76395f60d955cdee402
Spencer Gurnee <spencergurnee@gmail.com>
2023-10-01 12:34:55 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solutions in this file
+// for (let age = 30; age < 40; age++){
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//     // debugger;
+// }
+
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts){
+//     for (let i = 0; i < gifts.length; i++){
+//         console.log(`Wrapped ${gifts[i]} and a bow!`);
+//         // debugger;
+//     }
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names) {
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+    const message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    messages.push(message);
+    console.log(message);
+  }
+  return messages;
+}
+
+writeCards(names);
+
+
+function countDown(){
+  for (let countDown = 10; countDown >= 0; countDown--){
+    console.log(countDown);
+  }
+}
